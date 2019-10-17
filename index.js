033ab126c005784fb1174fe190d84c556ebd1b65
NaluFigueira <anachfigueira@gmail.com>
2019-10-17 11:10:16 -0300

    Desafio 1 concluído!

--- index.js
+++ index.js
@@ -3,18 +3,63 @@ const express = require('express');
 const server = express(); 
 server.use(express.json());
 
-projetos = [];
+projects = [];
+
+
+function checkIfProjectExists(req, res, next){
+  const {id} = req.params;
+
+  const index = projects.findIndex(project => project.id === id);
+
+  if(index === -1)
+    return res.status(400).json({error: "Projeto não encontrado!"})
+  else{
+    req.project = projects[index];
+    req.index = index;
+    return next();
+  }
+}
+
+server.get('/projects',(req,res) =>{
+  return res.json(projects);
+});
 
 server.post('/projects',(req,res) =>{
-  const {id,title,tasks} = req.body;
+  const {id,title} = req.body;
 
-  projetos.push({
+  projects.push({
     id,
     title,
-    tasks
+    tasks:[]
   });
 
-  return res.json(projetos);
+  return res.json(projects);
 });
+
+server.post('/projects/:id/tasks', checkIfProjectExists, (req,res) =>{
+  const {title} = req.body;
+  
+  req.project.tasks.push(title);
+  
+  return res.json(req.project);
+
+});
+
+server.put('/projects/:id', checkIfProjectExists,(req,res)=>{
+  const {title} = req.body;
+
+  req.project.title = title;
+  
+  return res.json(req.project);
+
+});
+
+server.delete('/projects/:id',checkIfProjectExists,(req,res)=>{
+
+  projects.splice(req.index,1);
+  
+  return res.send("Removido com sucesso!");
+  
+})
 
 server.listen(3000);
